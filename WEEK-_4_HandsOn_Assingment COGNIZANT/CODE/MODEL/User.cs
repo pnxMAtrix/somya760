60b809bd9680f8c3aa6a8675e8856927f5cb565b
SOMYA RANJAN SAHOO <sahoosoumya233@gmail.com>
2026-07-28 16:50:34 +0530

    Add User model for JWT authentication

---
+++ WEEK-_4_HandsOn_Assingment COGNIZANT/CODE/MODEL/User.cs
@@ -0,0 +1,10 @@
+namespace JwtAuthenticationDemo.Models;
+
+public class User
+{
+    public string Username { get; set; } = "";
+
+    public string Password { get; set; } = "";
+
+    public string Role { get; set; } = "";
+}
